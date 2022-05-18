9b01e6a05932d2dcab3ec0c8cc596669fcae5cbd
Tamerlan <tamer_idrisov98@mail.ru>
2022-05-18 21:51:01 +0300

    myFirstGame

---
+++ game.js
@@ -0,0 +1,50 @@
+let intocode9 = {
+    team: {
+    Askhab: {
+      name: 'mushinsky',
+      age: 21,
+      health: 100,
+      stamina: 77,
+      growthAbility: 99,
+      toxicism: 0,
+      comfort: 100,
+    },
+    Deni: {
+      name: 'bratka',
+      age: 24,
+      health: 75,
+      stamina: 8,
+      growthAbility: 27,
+      comfort: 100,
+      toxicism: 100
+    },
+    Israil: {
+      name: 'nash',
+      age: 24,
+      health: 95,
+      stamina: 95,
+      growthAbility: 95,
+      comfort: 100,
+      toxicism: 0
+    }, 
+    Imran: {
+      name: 'Stifmaster',
+      age: 24,
+      health: 92,
+      stamina: 88,
+      growthAbility: 100,
+      comfort: 100,
+      toxicism: 0
+    },
+  
+    Mansur: {
+      name: 'tehnar',
+      age: 19,
+      health: 95,
+      stamina: 100,
+      growthAbility: 100,
+      comfort: 100,
+      toxicism: 0
+    }
+  }
+}
